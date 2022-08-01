09af11ae46412c514b3aa71d5661af5f5cb9e98f
yasar2385 <yasar.mohideen@nkw.pub>
2022-08-01 22:33:44 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+const regexp = /t(e)(st(\d?))/g;
+const str = 'test1test2';
+
+const array = [...str.matchAll(regexp)];
+
+console.log(array[0]);
+// expected output: Array ["test1", "e", "st1", "1"]
+
+console.log(array[1]);
+// expected output: Array ["test2", "e", "st2", "2"]
